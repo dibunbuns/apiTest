56f34dbc20a6867f474e93200acc031badcd69f4
Dita Muller <dmuller@fnba.com>
2024-05-28 14:08:25 -0400

    [dmuller] first api with fetch!

---
+++ index.js
@@ -0,0 +1,32 @@
+const pokemonName = document.getElementById("pokemonName");
+const button = document.getElementById("button");
+
+button.addEventListener('click', fetchData);
+
+pokemonName.addEventListener('keydown', function(event){
+    if(event.key=== 'Enter'){
+        fetchData();
+    }
+})
+
+
+async function fetchData(){
+    try{
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        
+        if(!response.ok){
+            throw new Error("Could not fetch pokemon");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error){
+        console.error(error);
+    }
+}
